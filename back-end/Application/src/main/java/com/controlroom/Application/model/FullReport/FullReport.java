794b91a56b448693b4b22467a071700ad1e76c5c
Christos Skandalis <chris96skan@gmail.com>
2020-05-02 18:25:16 +0300

    create a repository and basic model for full reports service needs to be added and a full report dto one to one foreign to user (control room user-adder)

---
+++ back-end/Application/src/main/java/com/controlroom/Application/model/FullReport/FullReport.java
@@ -0,0 +1,39 @@
+package com.controlroom.Application.model.FullReport;
+
+import com.controlroom.Application.model.incidentModel.Incident;
+import com.controlroom.Application.model.userModel.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class FullReport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long report_Id;
+
+    private long injuries;
+    private long deaths;
+    @OneToOne(fetch = FetchType.LAZY)
+    private User adder;
+
+    public FullReport(int deaths, int injuries, User user) {
+        this.deaths = deaths;
+        this.injuries = injuries;
+        this.adder = user;
+    }
+
+    public FullReport(){
+
+    }
+}
