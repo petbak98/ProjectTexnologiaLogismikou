78417bd4a818634f7e77a10f72ddf98a4b164197
DKotsa <dimitris.kotsanitis@gmail.com>
2020-06-14 17:11:11 +0300

    CLI running with user prompt

--- cli-app/index.js
+++ cli-app/index.js
@@ -15,31 +15,41 @@ console.log(
 		)
 	);
 
-let command = process.argv[2];
+inquirer
+.prompt([
+{
+	name: 'command',
+	message: 'Please enter your command: ',
+},
+])
+.then(value => {
+	let command = String(value.command);
+	if(command === "health-check") {
+		healthCheck();
+	} else if (command === "login") {
+		inquirer
+		.prompt([
+		{
+			name: 'user',
+			message: 'Please enter your username: ',
+		},
+		{
+			type: 'password',
+			name: 'pass',
+			message: 'Please enter your password: ',
+		},
+		])
+		.then(answers => {
+			let user = String(answers.user);
+			let pass = String(answers.pass);
+			login(user,pass);
+		});
 
-if(command === "health-check") {
-	healthCheck();
-} else if (command === "login") {
-	inquirer
-	.prompt([
-	{
-		name: 'user',
-		message: 'Please enter your username: ',
-	},
-	{
-		type: 'password',
-		name: 'pass',
-		message: 'Please enter your password: ',
-	},
-	])
-	.then(answers => {
-		let user = String(answers.user);
-		let pass = String(answers.pass);
-		login(user,pass);
-	});
 
-	
-}
+	}
+});
+
+
 
 
 
